56a51b45fd0c8195ca7a5b8e32a71ca8067691f3
danielkng <koenning.daniel@gmail.com>
2021-04-12 18:46:44 +0200

    Removed GH-Lines

--- src/App.js
+++ src/App.js
@@ -18,12 +18,9 @@ function App() {
             <Route path="/telegram/send" exact component={send} />
             <Route path="/telegram/received" exact component={received} />
             <Route path="/telegram/deleted" exact component={deleted} />
-<<<<<<< HEAD
             <Route path="/tiktok/tiktok_send" exact component={tiktok_send} />
-=======
             <Route path="/tiktok/send" exact component={tiktok_send} />
             <Route path="/tiktok/send" exact component={tiktok_api} />
->>>>>>> c287d0f5df386e0c5d21cd2e5409697cfc02d794
           </Switch>
     </Router>
 
