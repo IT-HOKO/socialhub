2216832d9abb189970ead96e82f9dcc005cc028f
Dominik Homölle <homoelle.dominik@gmail.com>
2021-04-12 18:44:48 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import {home, users} from './pages/home';
 import {main, send, received, deleted} from './pages/telegram'
-import {tiktok_send} from './pages/tiktok'
-import {tiktok_api, tiktok_send} from './pages/tiktok'
+import {tiktok_send, tiktok_api} from './pages/tiktok'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
@@ -18,12 +17,8 @@ function App() {
             <Route path="/telegram/send" exact component={send} />
             <Route path="/telegram/received" exact component={received} />
             <Route path="/telegram/deleted" exact component={deleted} />
-<<<<<<< HEAD
-            <Route path="/tiktok/tiktok_send" exact component={tiktok_send} />
-=======
             <Route path="/tiktok/send" exact component={tiktok_send} />
             <Route path="/tiktok/send" exact component={tiktok_api} />
->>>>>>> c287d0f5df386e0c5d21cd2e5409697cfc02d794
           </Switch>
     </Router>
 
